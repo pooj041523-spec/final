af668f4aa28338f4d008799d0e7666721d8b2895
unknown <pooja041523@gmail.com>
2025-10-16 21:59:25 +0530

    Update titanic_practice.py with cleaning and analysis steps

--- titanic_practice.py
+++ titanic_practice.py
@@ -1,10 +1,31 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 df = pd.read_csv("train.csv")
-survival_counts =df["Survived"].value_counts()
-survival_counts.plot(kind="bar")
+#Inspect the dataset (head(),info(),describe())
+print("First 5 rows of the dataset")
+print(df.head())
+print("\nDataset info: ")
+print(df.info())
+print("\nstatistical summary :")
+print(df.describe())
+#clean the dataset
+df['Age'].fillna(df['Age'].mean(),inplace=True)
+df.drop(columns=['Cabin','Ticket','Name'],inplace=True)
+print("\nDataset after cleaning:")
+print(df.head())
+#Analysis with pandas
+survival_counts = df['Survived'].value_counts()
+print("\n Number of passengers survived vs not survived:")
+print(survival_counts)
+#average age grouped by gender
+average_age_gender = df.groupby('Sex')["Age"].mean()
+print("\nAverage age grouped by gender:")
+print(average_age_gender)
+#simple bar chart using matplotlib
+plt.figure(figsize=(6,4))
+survival_counts.plot(kind="bar",color=['red','green'])
 plt.title("survivors vs non-survivors")
-plt.xlabel("survival status")
+plt.xlabel("survival (0 = no,1 = yes)")
 plt.ylabel("number of passengers")
-plt.xticks(ticks=[0,1],labels=["not survived","survived"],rotation=0)
+plt.xticks(rotation=0)
 plt.show()
